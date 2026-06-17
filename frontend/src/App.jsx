168efbb61d67adab53826ece45d1ad1d7c8b49ae
adarsh2850 <madarshkumar2@gmail.com>
2026-06-17 15:20:50 +0530

    added context form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,18 @@ const App = () => {
       <li>AI in Agriculture</li>
       <li>AI in Transportaion</li>
     </ul>
+    <form action="#">
+      <label htmlFor="name">Name</label>
+      <input type="text" id='name' name='name' />
+      <br />
+      <label htmlFor="email">Email</label>
+      <input type="email" id='email' name='email' />
+      <br />
+      <label htmlFor="message">Message</label>
+      <textarea name="message" id="message"></textarea>
+      <br />
+      <button type='submit'>Submit</button>
+    </form>
     </>
   )
 }
