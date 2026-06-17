674cd9df9c4c6ee160944ac9deb3cca650743c9f
adarsh2850 <madarshkumar2@gmail.com>
2026-06-17 14:26:46 +0530

    intial build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    //Create a list for AI Blogs topic
+    <>
+    <div>AI Blogs</div>
+    <ul>
+      <li>AI Education</li>
+      <li>AI in Finanace</li>
+      <li>AI in HealthCare</li>
+      <li>AI in Agriculture</li>
+      <li>AI in Transportaion</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
